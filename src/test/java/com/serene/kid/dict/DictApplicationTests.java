97fea5abacbf9ea99fe40ac65809eeab51528830
nikola.yurukov <nikola.yurukov@scalefocus.com>
2016-10-12 11:00:54 +0300

    Initial commit

---
+++ src/test/java/com/serene/kid/dict/DictApplicationTests.java
@@ -0,0 +1,16 @@
+package com.serene.kid.dict;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DictApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
