680b2d34a84e7d2d597167f7e3b1e818eeffa0fb
nikola.yurukov <nikola.yurukov@scalefocus.com>
2016-10-13 18:06:23 +0300

    [Dev] Init front page of application

--- src/main/java/com/serene/kid/dict/controllers/WordController.java
+++ src/main/java/com/serene/kid/dict/controllers/WordController.java
@@ -1,12 +1,13 @@
 package com.serene.kid.dict.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.serene.kid.dict.repositories.WordRepository;
 
-@RestController
+@Controller
 public class WordController {
 
 	private final WordRepository wordRepository;
@@ -16,7 +17,13 @@ public class WordController {
 		this.wordRepository = wordRepository;
 	}
 
+	@RequestMapping("/")
+	public String getMainPage() {
+		return "index";
+	}
+
 	@RequestMapping("/words")
+	@ResponseBody
 	public String getAllWords() {
 		return wordRepository.findAll().stream().map(s -> s.toString()).reduce("", (a, b) -> a + " ;; " + b);
 	}
