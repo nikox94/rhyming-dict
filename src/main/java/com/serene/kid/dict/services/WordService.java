6025085f48868a9138eff405c4335dd8f6da3f88
Nikola Yurukov <nikola.yuroukov@gmail.com>
2016-11-11 18:44:46 +0200

    [Dev] Created parser for words + tests (some)

--- src/main/java/com/serene/kid/dict/services/WordService.java
+++ src/main/java/com/serene/kid/dict/services/WordService.java
@@ -5,13 +5,18 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
@@ -28,8 +33,8 @@ import lombok.extern.java.Log;
 public class WordService {
 
 	final String APACHE_DICT_URL = "http://downloads.sourceforge.net/project/aoo-extensions/744/8/dictionaries-bg.oxt";
-	final Path APACHE_DICT_TARGET_FILE = FileSystems.getDefault()
-			.getPath(System.getProperty("java.io.tmpdir"), "dictionaries-bg.oxt");
+	final Path APACHE_DICT_TARGET_FILE = FileSystems.getDefault().getPath(System.getProperty("java.io.tmpdir"),
+			"dictionaries-bg.oxt");
 
 	private File downloadApacheDictionaryFile() {
 		try {
@@ -37,10 +42,10 @@ public class WordService {
 			final URL url = new URL(APACHE_DICT_URL);
 			log.log(Level.INFO, "Target file for download is " + APACHE_DICT_TARGET_FILE);
 			final File target = new File(APACHE_DICT_TARGET_FILE.toString());
-			final WGet w = new WGet(url, target);
-			// single threaded download. Will return here only when
+			final WGet wget = new WGet(url, target);
+			// Single threaded download. Will return here only when
 			// download completes or error.
-			w.download();
+			wget.download();
 			log.log(Level.INFO, "Dictionary download from APACHE finished successfully.");
 			return target;
 		} catch (final MalformedURLException e) {
@@ -58,25 +63,50 @@ public class WordService {
 		File apacheDictZip = new File(APACHE_DICT_TARGET_FILE.toString());
 
 		if (!apacheDictZip.exists()) {
-			apacheDictZip = downloadApacheDictionaryFile(); // new File(APACHE_DICT_TARGET_FILE);
+			apacheDictZip = downloadApacheDictionaryFile(); // new
+															// File(APACHE_DICT_TARGET_FILE);
 		}
 
 		final List<Word> result = new ArrayList<>();
 
 		try {
 			final ZipFile zipFile = new ZipFile(apacheDictZip);
-			final ZipEntry dicFile = zipFile.stream().filter(s -> s.getName().equals("spell/bg_BG.dic")).findFirst().get();
-			// TODO: include aff file ending parsing
-			// final ZipEntry affFile = zipFile.stream().filter(s -> s.getName().equals("spell/bg_BG.aff")).findFirst().get();
+			final ZipEntry dicFile = zipFile.stream().filter(s -> s.getName().equals("spell/bg_BG.dic")).findFirst()
+					.get();
+			final ZipEntry affFile = zipFile.stream().filter(s -> s.getName().equals("spell/bg_BG.aff")).findFirst()
+					.get(); // Include aff file for ending parsing
 
 			final InputStream dicStream = zipFile.getInputStream(dicFile);
+			final InputStream affStream = zipFile.getInputStream(affFile);
 			final BufferedReader reader = new BufferedReader(new InputStreamReader(dicStream, "CP1251"));
+
+			final String dicRegex = "^(?<word>\\S+)/(?<affix>\\w)$";
+			final Pattern dicPattern = Pattern.compile(dicRegex);
+			final Map<String, List<AffixRule>> affixRules = parseAffixFile(affStream);
 
 			// Ignore first line. It contains the number of words in the file.
 			reader.readLine();
-			reader.lines().filter(s -> !s.contains("/")).forEach(s -> {
-				result.add(new Word(s));
-					});
+			reader.lines().forEach(s -> {
+				if (s.contains("/")) {
+					final Matcher matcher = dicPattern.matcher(s);
+					if (matcher.matches()) {
+						final String word = matcher.group("word");
+						final String affix = matcher.group("affix");
+						final List<AffixRule> rules = affixRules.get(affix);
+						for (final AffixRule rule : rules) {
+							if (rule.remove == "0")
+								result.add(new Word(word + rule.ending));
+							else if (rule.remove
+									.equals(word.substring(word.length() - rule.remove.length(), word.length()))) {
+								result.add(new Word(word.substring(0, word.length() - rule.remove.length())
+										+ (rule.ending.equals("0") ? "" : rule.ending)));
+							}
+						}
+					}
+				} else {
+					result.add(new Word(s));
+				}
+			});
 
 			zipFile.close();
 
@@ -87,6 +117,48 @@ public class WordService {
 		}
 
 		return result;
+	}
+
+	private Map<String, List<AffixRule>> parseAffixFile(final InputStream affStream) {
+		final HashMap<String, List<AffixRule>> result = new HashMap<>();
+		final String affRegex = "^(?<type>\\b\\S+\\b)\\s+(?<name>\\b\\S+\\b)\\s+(?<remove>\\b\\S+\\b)\\s+(?<ending>\\S+)\\s+(?<regex>\\S+)\\s*$";
+		final Pattern affPattern = Pattern.compile(affRegex);
+
+		try {
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(affStream, "CP1251"));
+			reader.lines().forEach(s -> {
+				final Matcher matcher = affPattern.matcher(s);
+				if (matcher.matches()) {
+					if (!result.containsKey(matcher.group("name"))) {
+						result.put(matcher.group("name"), new ArrayList<AffixRule>());
+					}
+					result.get(matcher.group("name")).add(matcherToAffixRule(matcher));
+				}
+			});
+
+		} catch (final UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	private AffixRule matcherToAffixRule(final Matcher matcher) {
+		final AffixRule result = new AffixRule();
+		result.type = matcher.group("type");
+		result.name = matcher.group("name");
+		result.remove = matcher.group("remove");
+		result.ending = matcher.group("ending");
+		result.regex = matcher.group("regex");
+		return result;
+	}
+
+	private class AffixRule {
+		public String type;
+		public String name;
+		public String remove;
+		public String ending;
+		public String regex;
 	}
 
 }
--- src/test/java/com/serene/kid/dict/DictApplicationTests.java
+++ src/test/java/com/serene/kid/dict/DictApplicationTests.java
@@ -1,16 +1,50 @@
 package com.serene.kid.dict;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class DictApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void checkIfCorrectlyParsedWords() {
+
+		final String body = this.restTemplate.getForObject("/words", String.class);
+		//assertThat(body).contains(" демодулирам ");
+		assertThat(body).contains(" демодулира ");
+		assertThat(body).contains(" демодулирахме ");
+		assertThat(body).contains(" демодулиращ ");
+		assertThat(body).contains(" демодулиращият ");
+		assertThat(body).doesNotContain(" демодулираейки ");
+		assertThat(body).doesNotContain(" демодулираим ");
+
+		//assertThat(body).contains(" демон ");
+		//assertThat(body).contains(" демона ");
+		//assertThat(body).contains(" демони ");
+		//assertThat(body).contains(" демоните ");
+		//assertThat(body).contains(" демонът ");
+
+
+		//assertThat(body).contains(" ящен ");
+		assertThat(body).contains(" ящна ");
+		assertThat(body).doesNotContain(" ящеите ");
+
+		assertThat(body).contains(" Абърдийн ");
+		assertThat(body).doesNotContain("78233");
+	}
 }
